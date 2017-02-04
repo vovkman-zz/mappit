07900c4c7675c4d662ca3191b5600a15e08aaf8e
Fredrick <fredrickleber@gmail.com>
2017-02-04 10:15:26 -0500

    Added .gitignore.

--- background.js
+++ background.js
@@ -3,7 +3,10 @@
 function onClickHandler(info, tab) {
     if (info.menuItemId == "mappit") {
         console.log("mappit was clicked.");
-        alert(info["selectionText"]);
+        //alert(info["selectionText"]);
+
+        chrome.windows.create({"url" : "https://www.google.com/maps/embed/v1/place?key=AIzaSyBYrE1eJS0bbprf3-87rihnx-kYJcK40xI    &q=" + info["selectionText"],
+            "height" : 300, "width" : 500, "type" : "popup"})
     }
 }
 
@@ -14,3 +17,11 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({"title" : "mappit", "id": "mappit", "contexts" :["selection"]});
     console.log("Created mappit menu item.");
 });
+
+
+
+// HTML API key: AIzaSyBYrE1eJS0bbprf3-87rihnx-kYJcK40xI
+// JS API key: AIzaSyD0n19aMeQbPiWHFTc08PiNNjf4v7ZXgHQ
+
+
+
