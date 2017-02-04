f2bac973acead0769bc2081bb5bfb356c56505e9
Fredrick <fredrickleber@gmail.com>
2017-02-04 01:10:12 -0500

    Messed up previous commit, this is the real deal.

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
-/**
- * Created by fredrick on 2/3/17.
- */
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+    if (info.menuItemId == "mappit") {
+        console.log("mappit was clicked.");
+        alert(info["selectionText"]);
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({"title" : "mappit", "id": "mappit", "contexts" :["selection"]});
+    console.log("Created mappit menu item.");
+});
